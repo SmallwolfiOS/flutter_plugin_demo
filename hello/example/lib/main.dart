1935a9dec9bc67b72be73b119e23538b07f29eb5
SmallwolfiOS <1348748408@qq.com>
2020-02-03 17:07:57 +0800

    add flutter code tmp

--- hello/example/lib/main.dart
+++ hello/example/lib/main.dart
@@ -23,13 +23,15 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
+    int sumNumber;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await Hello.platformVersion;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
-
+    sumNumber = Hello.calculate(1, 2);
+    print(sumNumber);
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
--- hello/lib/hello.dart
+++ hello/lib/hello.dart
@@ -10,4 +10,9 @@ class Hello {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+  //这个是直接从flutter层面封装，不需要channel进行桥接
+  static int calculate (int a, int b){
+    return a + b;
+  }
+
 }
